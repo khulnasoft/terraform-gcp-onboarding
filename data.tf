040799fad2307b14ee03b6d779717e66073f5af5
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-11 19:58:01 +0600

    Create data.tf
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ data.tf
@@ -0,0 +1,12 @@
+# data.tf
+
+# Retrieve information about the Google Cloud organization
+data "google_organization" "organization" {
+  count  = var.dedicated_project ? 1 : 0
+  domain = var.org_name
+}
+
+# Retrieve information about the root Google Cloud project
+data "google_project" "project" {
+  project_id = var.project_id
+}
